c3577307e2bd9904dc6ef92f89e7c2af01410f70
Débora Oliveira <98doliveira@gmail.com>
2022-03-07 15:46:24 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -30,6 +30,27 @@ void	print_stacks(t_list *stack_a, t_list *stack_b)
 	t_list	*ptr_b;
 
 	ptr_a = stack_a;
+	ptr_b = stack_b;
+	while (ptr_a || ptr_b)
+	{
+		if (ptr_a && ptr_b)
+		{
+			ft_printf("%s\t%s\n", ptr_a->content, ptr_b->content);
+			ptr_a = ptr_a->next;
+			ptr_b = ptr_b->next;
+		}
+		else if (ptr_a)
+		{
+			ft_printf("%s\t\n",  ptr_a->content);
+			ptr_a = ptr_a->next;		
+		}
+		else
+		{
+			ft_printf("  \t%s\n",  ptr_b->content);
+			ptr_b = ptr_b->next;
+		}		
+	}
+}
 
 int main(int argc, char **argv)
 {
@@ -40,4 +61,5 @@ int main(int argc, char **argv)
 	if (init_stack(argc, argv, &stack_a) != 1)
 		return (-1);
 	ft_lstclear(&stack_a, &free);
+	ft_lstclear(&stack_b, &free);
 }
